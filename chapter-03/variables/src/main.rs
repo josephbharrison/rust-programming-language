2a782e6dca236e0d652b2285b59e635841a0d941
Joe Harrison <josephbharrison@gmail.com>
2023-10-15 11:47:44 -0400

    adding ch 4 ownership

--- chapter-03/variables/src/main.rs
+++ chapter-03/variables/src/main.rs
@@ -1,7 +1,22 @@
 fn main() {
     // let x = 5; will error, vars immutable by default
     let mut x = 5; // make x mutable;
-    println!("The value of x is: (x)");
+    println!("The value of x is: {x}");
     x = 6;
-    println!("The value of x is: (x)");
+    println!("The value of x is: {x}");
+
+    // Data Types
+
+    let guess: u32 = "42".parse().expect("Not a number!");
+    println!("guess: {guess}");
+
+    // Floating points
+
+    let x = 2.0;
+
+    let y: f32 = 3.0;
+
+    println!("{x} {y}");
+
+
 }
